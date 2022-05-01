cfbaec00de1e81458e2aa7dcc59e31cdeceebe76
andresmendez <fmendez1212@gmail.com>
2022-04-30 23:07:40 -0500

    functional test class

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+import unittest
 
-browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        assert 'To-Do' in self.browser.title
+
+if __name__ == '__main__':
+    unittest.main()
